bb4057bc86d325ac59c338a62360dcffb605214b
JesusB <bejaranojesus7@gmail.com>
2012-09-01 17:49:26 -0430

    Initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warui::Application.config.secret_token = 'f2fba309a1405f458474ee0ef20195c8b6b03535d8acf4a6d22f35be7d326656d039487c5b3785ada1bb563739a36570bc3691b2282ec2205c23275e6370e034'
